6bd75bcaac81083150d7f61282c7debf45b97e2f
nghiaPTClassFunc <100978931+jenazu@users.noreply.github.com>
2023-03-03 21:28:58 +0700

    configs axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
+import { axiosConfigs } from "./configs/axios";
 import { browserRouters } from "./routes";
 
 const App: React.FC = () => {
+  // axiosConfigs();
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <RouterProvider router={browserRouters} />
---
+++ src/configs/axios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:1337/api";
+
+export const axiosConfigs = () => {
+  const token = localStorage.getItem("token");
+  //REQUEST
+  axios.interceptors.request.use(
+    async (config: any) => {
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      // if (error && error.request) {}
+      return Promise.reject(error);
+    }
+  );
+
+  //RESPONSE
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async function (error) {
+      return Promise.reject(error);
+    }
+  );
+};
