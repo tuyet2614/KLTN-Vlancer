ae8bf15203f01cfc1999c9106bbd5e5987bd6f89
tuyetna-comartek <tuyetna@comartek.com>
2023-03-16 17:36:48 +0700

    fix login

--- src/pages/auth/components/loginRouteComponent.tsx
+++ src/pages/auth/components/loginRouteComponent.tsx
@@ -1,23 +1,27 @@
 import { Button, Col, Divider, Form, Image, Row } from "antd";
-import { Fragment } from "react";
+import { Fragment, useCallback, useState } from "react";
 import faceIcon from "@assets/images/icon/fb-icon.png";
 import googleIcon from "@assets/images/icon/google-icon.png";
 import linkedinIcon from "@assets/images/icon/linkedin.png";
-// import { GoogleLogin } from "react-google-login";
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-const handleGoogleLoginSuccess = (response: any) => {
-  // TODO: get user data from response
-  alert(`Welcome ${response.profileObj.name}!`);
-};
-
-const handleGoogleLoginFailure = (response: any) => {
-  // TODO: handle login failure
-  console.log("response: ", response);
-};
+import { LoginSocialGoogle, IResolveParams } from "reactjs-social-login";
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  GithubLoginButton,
+  LinkedInLoginButton,
+} from "react-social-login-buttons";
 
 const LoginRouteComponent = () => {
   const clientId =
     "1083253828981-fks1m2ggmp4sgdf0rk60n6p5683tmpfg.apps.googleusercontent.com";
+  const [provider, setProvider] = useState("");
+  const [profile, setProfile] = useState<any>();
+
+  const onLoginStart = useCallback(() => {
+    alert("login start");
+  }, []);
+
+  const REDIRECT_URI = "http://localhost:3002/auth/login";
   return (
     <Fragment>
       <div>
@@ -35,19 +39,25 @@ const LoginRouteComponent = () => {
         <Row className="flex justify-between ">
           <Col span={11}>
             <Form.Item className="login-item">
-              {/* <Button>
-                    <Image src={googleIcon} preview={false} className="icon" />
-                    <span className="text-[#757575]">Google</span>
-                  </Button> */}
-              <GoogleOAuthProvider clientId={clientId}>
-                <GoogleLogin
-                  // clientId={clientId}
-                  // buttonText="Log in with Google"
-                  onSuccess={handleGoogleLoginSuccess}
-                  // onFailure={handleGoogleLoginFailure}
-                  // cookiePolicy={"single_host_origin"}
-                />
-              </GoogleOAuthProvider>
+              <LoginSocialGoogle
+                client_id={clientId || ""}
+                onLoginStart={onLoginStart}
+                redirect_uri={REDIRECT_URI}
+                scope="openid profile email"
+                discoveryDocs="claims_supported"
+                access_type="offline"
+                onResolve={({ provider, data }: IResolveParams) => {
+                  console.log("success");
+
+                  setProvider(provider);
+                  setProfile(data);
+                }}
+                onReject={(err) => {
+                  console.log(err);
+                }}
+              >
+                <GoogleLoginButton />
+              </LoginSocialGoogle>
             </Form.Item>
           </Col>
           <Col span={11}>
